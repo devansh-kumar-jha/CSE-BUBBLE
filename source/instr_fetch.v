8d68e50679a64ddd815372d40c8fdcee905e68e7
Shivangp4 <83136126+Shivangp4@users.noreply.github.com>
2023-03-22 04:13:23 +0530

    Implemented instruction fetch phase

--- source/instr_fetch.v
+++ source/instr_fetch.v
@@ -3,6 +3,25 @@
 // The module takes input the program counter and the instruction memory from the processor
 // and outputs the instruction in IR register
 
-module instr_fetch();
+
+module instr_fetch(
+  input clk,                                // Clock signal
+  input reset,                              // Reset signal
+  input [31:0] pc;                          // Program Counter
+  input [31:0] instruction_memory [255:0];  // Instruction_memory
+  output reg [31:0] ir                      // Instruction Register
+);
+
+  // Always block for instruction fetch
+  always @(posedge clk, posedge reset) begin
+    if (reset) begin
+      // Reset the instruction register
+      ir <= 32'h0;
+    end
+    else begin
+      // Fetch the instruction at the current program counter
+      ir <= instruction_memory[pc];
+    end
+  end
 
 endmodule
